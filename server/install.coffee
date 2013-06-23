6c6309205b9b472c8e74aadfe70758a7e5250aa7
Jeromy Maligie <jmaligie@gmail.com>
2013-06-22 20:47:21 -0700

    :3

--- server/install.coffee
+++ server/install.coffee
@@ -3,52 +3,74 @@ util = require('util')
 sys = require('sys')
 exec = require('child_process').exec;
 fs = require('fs')
+os = require('os')
 
 
-puts_console = (error, stdout, stderr, callback) -> 
+
+
+puts_console = (error, stdout, stderr, callback) => 
 	sys.puts(stdout)
 	callback
 
-puts = (obj) ->
+puts = (obj) =>
 	util.puts(obj)
 
-center = (string,delim,times) ->
+puts """ Hello there stranger, you seem to be using Windows to run this here highly optimized server platform. This is not reccomended or supported at this time!
+if you'd like to run GameLab, please visit VirtualBox.com, virtualize a Linux distribution, and install this package again! We'll be waiting patiently until you do :)
+""" if os.platform() is "win32"
+
+center = (string,delim,times) =>
     a = (delim for i in [0..times/2])
     b = (delim for i in [0..times/2])
     puts(a.join("")+string+b.join(""))
 
-npm_install = (module, global, callback) ->
+npm_install = (module, global, callback) =>
 	if fs.exists("#{__dirname}/node_modules")
 		puts("module #{module} is already installed locally! Check the '#{__dirname}/node_modules' folder if you have issues")
 	else if fs.exists("#{process.cwd}/node_modules")
 		puts("module #{module} is already installed locally! Check the '#{process.cwd}/node_modules' folder if you have issues")
 	else
-		puts("made it to the installer..")
-		global = "" if global == null
+		switch global
+			when null
+				global = ""
+			when true
+				global = "-g"
+			when false
+				global = ""
+			else 
+				global = ""
 		center("Installing #{module}","!",10)
-		exec("npm install #{module} #{global}")
-		callback if callback
+		exec("npm install #{module} #{global}", 
+			(stdout, stderr, error) => 
+				sys.print(stdout)
+				sys.print(stderr)
+				console.log(error) if error
+				callback()
+			)
 
 
 center("Installing NPM Modules!","!",10)
 
 modules = {"coffee-script": true, "iced-coffee-script": true, "requirejs": false, "socket.io": false, "watch": false}
 
-install_list = (list,index) ->
-	puts list
-	puts index
-	npm_install(Object.keys(list)[index], list[index], install_list(list, index+1) ) if index >= Object.keys(list).length
+install_list = (list,index) =>
+	npm_install(Object.keys(list)[index], list[index], () => install_list(list, index+1) ) if index < Object.keys(list).length
+	if index == Object.keys(list).length
+		we_done_now()
+
+
+we_done_now = () =>
+	center("Done","-",10)
+	puts( """
+	 README
+	 
+	 If you have an 'EROFS' error on a writable fs,
+	 you need to tell VirtualBox to allow Symlinks on shared directories.
+	 See https://www.virtualbox.org/ticket/10085#comment:12 for details
+
+	 Installation Complete! Have a great day and happy hacking! - Gamelab""")
 
 
 install_list(modules, 0)
 
 
-center("Done","-",10)
-puts( """
- README
- 
- If you have an 'EROFS' error on a writable fs,
- you need to tell VirtualBox to allow Symlinks on shared directories.
- See https://www.virtualbox.org/ticket/10085#comment:12 for details
-
- Installation Complete! Have a great day and happy hacking! - Gamelab""")
--- server/install.js
+++ server/install.js
@@ -1,4 +1,5 @@
-var center, exec, fs, install_list, modules, npm_install, puts, puts_console, sys, util;
+var center, exec, fs, install_list, modules, npm_install, os, puts, puts_console, sys, util, we_done_now,
+  _this = this;
 
 util = require('util');
 
@@ -8,16 +9,20 @@ exec = require('child_process').exec;
 
 fs = require('fs');
 
+os = require('os');
+
 puts_console = function(error, stdout, stderr, callback) {
   sys.puts(stdout);
-  if (callback) {
-  	callback();
-  }
+  return callback;
 };
 
 puts = function(obj) {
   return util.puts(obj);
 };
+
+if (os.platform() === "win32") {
+  puts(" Hello there stranger, you seem to be using Windows to run this here highly optimized server platform. This is not reccomended or supported at this time!\nif you'd like to run GameLab, please visit VirtualBox.com, virtualize a Linux distribution, and install this package again! We'll be waiting patiently until you do :)");
+}
 
 center = function(string, delim, times) {
   var a, b, i;
@@ -46,21 +51,28 @@ npm_install = function(module, global, callback) {
   } else if (fs.exists("" + process.cwd + "/node_modules")) {
     return puts("module " + module + " is already installed locally! Check the '" + process.cwd + "/node_modules' folder if you have issues");
   } else {
-    if (global === null) {
-      global = "";
-    }
-    else if (global === true) {
-    	global = "-g";
-    }
-    else if (global === false) {
-    	global = "";
-    }
-    else {
-    	global = "";
+    switch (global) {
+      case null:
+        global = "";
+        break;
+      case true:
+        global = "-g";
+        break;
+      case false:
+        global = "";
+        break;
+      default:
+        global = "";
     }
     center("Installing " + module, "!", 10);
-    puts("npm install " + module + " "+ callback)
-    exec("npm install " + module, callback());
+    return exec("npm install " + module + " " + global, function(stdout, stderr, error) {
+      sys.print(stdout);
+      sys.print(stderr);
+      if (error) {
+        console.log(error);
+      }
+      return callback();
+    });
   }
 };
 
@@ -75,13 +87,19 @@ modules = {
 };
 
 install_list = function(list, index) {
-  if (index <= Object.keys(list).length) {
-    npm_install(Object.keys(list)[index], list[index], install_list(list, index + 1));
+  if (index < Object.keys(list).length) {
+    npm_install(Object.keys(list)[index], list[index], function() {
+      return install_list(list, index + 1);
+    });
+  }
+  if (index === Object.keys(list).length) {
+    return we_done_now();
   }
 };
 
+we_done_now = function() {
+  center("Done", "-", 10);
+  return puts("README\n\nIf you have an 'EROFS' error on a writable fs,\nyou need to tell VirtualBox to allow Symlinks on shared directories.\nSee https://www.virtualbox.org/ticket/10085#comment:12 for details\n\nInstallation Complete! Have a great day and happy hacking! - Gamelab");
+};
+
 install_list(modules, 0);
-
-center("Done", "-", 10);
-
-puts("README\n\nIf you have an 'EROFS' error on a writable fs,\nyou need to tell VirtualBox to allow Symlinks on shared directories.\nSee https://www.virtualbox.org/ticket/10085#comment:12 for details\n\nInstallation Complete! Have a great day and happy hacking! - Gamelab");
